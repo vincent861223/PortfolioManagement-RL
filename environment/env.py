34e9d7a6f850824b22e026e956ed1cfce92d8dfe
vincent861223 <b05505019@meow1.csie.ntu.edu.tw>
2020-04-29 20:03:29 +0800

    initialize repository

---
+++ environment/env.py
@@ -0,0 +1,9 @@
+class Env():
+    def __init__(self, datapath):
+        self.datapath = datapath
+    
+    def seed(self, _seed):
+        raise NotImplementedError()
+
+
+    
---
+++ test.py
@@ -0,0 +1,7 @@
+
+from environment.env import Env
+
+
+if __name__ == '__main__':
+    env = Env("testpath");
+    env.seed(10)
